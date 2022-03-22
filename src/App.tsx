b4863aeb2342c77c49ac422b4ddaf404b2eb9495
michael <michael@arcturus.co>
2022-03-22 14:35:21 -0400

    test 5

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          TEST TEST AGAIN x4 <code>src/App.tsx</code> and save to reload.
+          TEST TEST AGAIN x5 <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
